de11af8afd85123de8240d47b6f31a4b94abb334
ichjames <ichjamesu@foxmail.com>
2025-10-23 13:38:21 +0800

    ``` feat(config): 更新数据库配置信息
    修改了 MySQL 数据库的连接配置，将主机地址、用户名和密码更新为新的生产环境配置。同时将 config.py 文件添加到 .gitignore 中以避免敏感信息泄露。
    ```

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # Database classicrealmd configuration
-host_account = "localhost"
-user_account = "user"   # 替换为你的 MySQL  用户名
-password_account = "123456"   # 替换为你的 MySQL 密码
+host_account = "10.147.18.11"
+user_account = "root"   # 替换为你的 MySQL  用户名
+password_account = "Vesoft!@#890"   # 替换为你的 MySQL 密码
 database_account = "classicrealmd"
 
 # Database classiccharacters configuration
-host_characters = "localhost"
-user_characters = "user"   # 替换为你的 MySQL  用户名
-password_characters = "123456"   # 替换为你的 MySQL 密码
+host_characters = "10.147.18.11"
+user_characters = "root"   # 替换为你的 MySQL  用户名
+password_characters = "Vesoft!@#890"   # 替换为你的 MySQL 密码
 database_characters = "classiccharacters"
